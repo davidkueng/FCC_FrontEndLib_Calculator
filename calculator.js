58780eeef081e004b9ad7ca7c47f5b6698c7d844
David <davidkueng@gmail.com>
2018-12-03 22:11:10 +0100

    js roadmap

---
+++ calculator.js
@@ -0,0 +1,9 @@
+// if a button is pressed -> push number from value into a display array (innerHTML from id with whitespace)
+
+// if clear is pressed -> delete all values in display array
+
+// if equals is pressed -> do the math of array and push result with calculation into array (reduce?)
+
+// make sure to keep the whole calculations in array with the result.
+
+// make sure that if there are several operators (+-*/) only the only the last one counts
