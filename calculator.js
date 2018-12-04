26921dcb29f154178636fa16e51f4a6395963ca3
David <davidkueng@gmail.com>
2018-12-04 18:25:42 +0100

    some refactoring

--- calculator.js
+++ calculator.js
@@ -7,7 +7,6 @@ for (let item of numbers) {
         item.addEventListener('click', (e) => {
             let value = e.srcElement.innerHTML;
             displayString += value;
-            console.log(displayString)
             document.getElementById('display').innerHTML = displayString
     })    
 };
@@ -17,18 +16,15 @@ let operators = document.getElementsByClassName("operatorInput");
 for (let item of operators) {
         item.addEventListener('click', (e) => {
             let value = e.srcElement.innerHTML;
-            console.log(displayString.slice(0))
             if (displayString.slice(-2).includes('+') || displayString.slice(-2).includes('-')
                 || displayString.slice(-2).includes('*') || displayString.slice(-2).includes('/')) {
                 let tempString = [...displayString]
                 tempString.splice(-2,1, value)
                 displayString = tempString.join('')
-                document.getElementById('display').innerHTML = displayString
             } else {
             displayString += ' ' + value + ' ';
-            document.getElementById('display').innerHTML = displayString
-            console.log(displayString)
             }
+            document.getElementById('display').innerHTML = displayString
     })    
 }
 
