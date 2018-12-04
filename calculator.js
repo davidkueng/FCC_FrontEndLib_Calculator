c7c6c1c1f7f2b461e43a6085ccabb77b9a0d8e79
David <davidkueng@gmail.com>
2018-12-04 13:33:04 +0100

    got simple operations with display output working

--- calculator.js
+++ calculator.js
@@ -7,3 +7,36 @@
 // make sure to keep the whole calculations in array with the result.
 
 // make sure that if there are several operators (+-*/) only the only the last one counts
+
+
+let displayArray = [];
+
+let buttonsId = document.getElementsByClassName("validInput");
+
+for (let item of buttonsId) {
+        item.addEventListener('click', (e) => {
+        let value = e.srcElement.innerHTML;
+        displayArray.push(value);
+        console.log(displayArray)
+        document.getElementById('display').innerHTML = displayArray
+    })
+    
+};
+
+document.getElementById('equals').addEventListener('click', (e) => {
+   document.getElementById('display').innerHTML = eval(displayArray.join(' '));
+})
+
+
+
+
+// let testArray = ['20', '+', '6', '*', '+', '10'];
+
+
+// console.log(eval(testArray.join(' ')))
+
+// console.log(20+6*+10)
+
+// PROBLEM: if multiple operators are entered, only use last one (maybe add classes and exception in buttons for of loop)
+
+
