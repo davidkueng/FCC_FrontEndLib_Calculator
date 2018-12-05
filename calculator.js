468507dd61e6c9bc163d7fae1b16e31913602d5d
David <davidkueng@gmail.com>
2018-12-05 16:09:33 +0100

    fixed double decimal problem

--- calculator.js
+++ calculator.js
@@ -6,7 +6,16 @@ let numbers = document.getElementsByClassName("numberInput");
 for (let item of numbers) {
         item.addEventListener('click', (e) => {
             let value = e.srcElement.innerHTML;
-            displayString += value;
+            let singleInput = displayString.slice(-1)
+            if (singleInput.includes('0') && displayString.length === 1 && value != '.') {
+                let tempArr = [...displayString]
+                tempArr.splice(-1,1, value)
+                displayString = tempArr.join('')
+            } else if (value ==='.' && displayString.includes('.')) {                
+                displayString = displayString
+            } else {
+                displayString += value;
+            }
             document.getElementById('result').innerHTML = value
             document.getElementById('display').innerHTML = displayString
     })    
@@ -17,9 +26,9 @@ let operators = document.getElementsByClassName("operatorInput");
 for (let item of operators) {
         item.addEventListener('click', (e) => {
             let value = e.srcElement.innerHTML;
-            let doubleOp = displayString.slice(-2);
-            if (doubleOp.includes('+') || doubleOp.includes('-')
-                || doubleOp.includes('*') || doubleOp.includes('/')) {
+            let singleInput = displayString.slice(-2);
+            if (singleInput.includes('+') || singleInput.includes('-')
+                || singleInput.includes('*') || singleInput.includes('/')) {
                 let tempArr = [...displayString]
                 tempArr.splice(-2,1, value)
                 displayString = tempArr.join('')
@@ -38,6 +47,17 @@ document.getElementById('equals').addEventListener('click', (e) => {
    displayString = result + ' ';
 });
 
-document.getElementById('AC').addEventListener('click', (e) => {
-    window.location.reload();
+document.getElementById('clear').addEventListener('click', (e) => {
+    document.getElementById('display').innerHTML = 0;
+    displayString = '';
+    document.getElementById('result').innerHTML = 0;
+    resultString = ''
 });
+
+
+// Tests not passing:
+// ===============
+// - decimal can only be entered once
+// - operations with decimal point
+// - several operators entered -> only last counts
+// - pressing = shows result and you can perform new calculation
